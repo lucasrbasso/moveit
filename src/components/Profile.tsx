b1eb9034cae022e66a18ceec8f8fd3a9c9785c09
Lucas Basso <lucasrbasso@users.noreply.github.com>
2021-02-23 16:14:36 -0300

    profile picture and description component

---
+++ src/components/Profile.tsx
@@ -0,0 +1,18 @@
+import styles from '../styles/components/Profile.module.css'
+
+export function Profile() {
+    return(
+        <div className={styles.profileContainer}>
+            <img 
+                src="https://media-exp1.licdn.com/dms/image/C4E03AQHxKFKD6u-68A/profile-displayphoto-shrink_800_800/0/1610828999294?e=1619654400&v=beta&t=Miq4Q0W8yFWIjlwns3HaX4apy6uOxnO527xhx5zN0o0" 
+                alt="Lucas Basso"/>
+            <div>
+                <strong>Lucas Basso</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level"></img>
+                    Level 1
+                </p>
+            </div>
+        </div>
+    );
+}
