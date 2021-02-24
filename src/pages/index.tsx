5a6a6b4c5e2572bbf8563c200a5d918656455a38
Lucas Basso <lucasrbasso@users.noreply.github.com>
2021-02-24 13:53:40 -0300

    updating index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,17 @@
-import React from "react";
-
-import { CompletedChallenges } from "../components/CompletedChallenges";
-import { Countdown } from "../components/Countdown";
-import ExperienceBar from "../components/ExperienceBar";
-import { Profile } from '../components/Profile';
+import React from 'react';
 
 import Head from 'next/head';
+import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Countdown } from '../components/Countdown';
+import ExperienceBar from '../components/ExperienceBar';
+import { Profile } from '../components/Profile';
 
 import styles from '../styles/pages/Home.module.css';
+import { ChallengeBox } from '../components/ChallengeBox';
 
 export default function Home() {
     return (
         <div className={styles.container}>
-
             <Head>
                 <title>Início | move.it</title>
             </Head>
@@ -21,13 +20,14 @@ export default function Home() {
 
             <section className={styles.section}>
                 <div>
-                    <Profile/>
-                    <CompletedChallenges/>
-                    <Countdown/>
-                </div> 
+                    <Profile />
+                    <CompletedChallenges />
+                    <Countdown />
+                </div>
                 <div>
+                    <ChallengeBox />
                 </div>
             </section>
         </div>
-    )
+    );
 }
