556915ebca97fee9f158d33c2f88149ce6f49d73
Lucas Basso <lucasrbasso@users.noreply.github.com>
2021-02-23 22:45:48 -0300

    eslint changes

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,15 +1,16 @@
-import styles from '../styles/components/Profile.module.css'
+import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
-    return(
+    return (
         <div className={styles.profileContainer}>
-            <img 
-                src="https://media-exp1.licdn.com/dms/image/C4E03AQHxKFKD6u-68A/profile-displayphoto-shrink_800_800/0/1610828999294?e=1619654400&v=beta&t=Miq4Q0W8yFWIjlwns3HaX4apy6uOxnO527xhx5zN0o0" 
-                alt="Lucas Basso"/>
+            <img
+                src="https://media-exp1.licdn.com/dms/image/C4E03AQHxKFKD6u-68A/profile-displayphoto-shrink_800_800/0/1610828999294?e=1619654400&v=beta&t=Miq4Q0W8yFWIjlwns3HaX4apy6uOxnO527xhx5zN0o0"
+                alt="Lucas Basso"
+            />
             <div>
                 <strong>Lucas Basso</strong>
                 <p>
-                    <img src="icons/level.svg" alt="Level"></img>
+                    <img src="icons/level.svg" alt="Level" />
                     Level 1
                 </p>
             </div>
