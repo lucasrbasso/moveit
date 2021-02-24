b2c27b56694eb67b95f2d5945a955bf1d5271de5
Lucas Basso <lucasrbasso@users.noreply.github.com>
2021-02-23 22:45:42 -0300

    updating start and stop

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,30 +1,33 @@
 import { useState, useEffect } from 'react';
-import styles from '../styles/components/Countdown.module.css'
+import styles from '../styles/components/Countdown.module.css';
 
 export function Countdown() {
-
     const [time, setTime] = useState(25 * 60);
     const [active, setActive] = useState(false);
 
-    const minutes = Math.floor(time/60);
+    const minutes = Math.floor(time / 60);
     const seconds = time % 60;
 
-    const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
-    const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+    const [minuteLeft, minuteRight] = String(minutes)
+        .padStart(2, '0')
+        .split('');
+    const [secondLeft, secondRight] = String(seconds)
+        .padStart(2, '0')
+        .split('');
 
     function startCountdown() {
         setActive(true);
     }
 
     useEffect(() => {
-        if(active && time > 0){
+        if (active && time > 0) {
             setTimeout(() => {
-                setTime(time - 1)
-            }, 1000)
+                setTime(time - 1);
+            }, 1000);
         }
     }, [active, time]);
 
-    return(
+    return (
         <div>
             <div className={styles.countdownContainer}>
                 <div>
@@ -38,8 +41,8 @@ export function Countdown() {
                 </div>
             </div>
 
-            <button 
-                type="button" 
+            <button
+                type="button"
                 className={styles.countdownButton}
                 onClick={startCountdown}
             >
