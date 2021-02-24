3ff495dea78d00abd1f531349fea591e71224824
Lucas Basso <lucasrbasso@users.noreply.github.com>
2021-02-24 13:54:01 -0300

    bug context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-props-no-spreading */
+
 import '../styles/global.css';
 
+import { ChallengesProvider } from '../contexts/ChallengesContext';
+
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
