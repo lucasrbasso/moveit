fcd05b2d511532ace6615de709bd3c9ec4e0b24a
Lucas Basso <lucasrbasso@users.noreply.github.com>
2021-02-26 16:51:57 -0300

    using cookies

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,6 +1,9 @@
 /* eslint-disable no-new */
-import { useState, createContext, ReactNode, useEffect } from 'react';
+import React, { useState, createContext, ReactNode, useEffect } from 'react';
+import Cookies from 'js-cookie';
 import challenges from '../../challenges.json';
+import { LevelUpModal } from '../components/LevelUpModal';
+import LoginScreen from '../pages/LoginScreen';
 
 interface Challenge {
     type: 'body' | 'eye';
@@ -13,35 +16,77 @@ interface ChallengesContextData {
     currentExperience: number;
     challengesCompleted: number;
     experienceToNextLevel: number;
+    name: string;
     activeChallenge: Challenge;
     levelUp: () => void;
     startNewChallenge: () => void;
     resetChallenge: () => void;
     completeChallenge: () => void;
+    closeLevelUpModal: () => void;
+    inputName: (event) => void;
+    closeLoginScreen: () => void;
 }
 
 interface ChallengesProviderProps {
+    name: string;
     children: ReactNode;
+    level: number;
+    currentExperience: number;
+    challengesCompleted: number;
 }
 
 export const ChallengesContext = createContext({} as ChallengesContextData);
 
-export function ChallengesProvider({ children }: ChallengesProviderProps) {
-    const [level, setLevel] = useState(1);
-    const [currentExperience, setCurrentExperience] = useState(0);
-    const [challengesCompleted, setChallengesCompleted] = useState(0);
-
+export function ChallengesProvider({
+    children,
+    ...rest
+}: ChallengesProviderProps) {
+    const [name, setName] = useState(rest.name ?? '');
+    const [level, setLevel] = useState(rest.level ?? 1);
+    const [currentExperience, setCurrentExperience] = useState(
+        rest.currentExperience ?? 0,
+    );
+    const [challengesCompleted, setChallengesCompleted] = useState(
+        rest.challengesCompleted ?? 0,
+    );
     const [activeChallenge, setActiveChallenge] = useState(null);
+    const [isLevelUpModalOpen, setIsLevelUpModalOpen] = useState(false);
+    const [isLoginScreenOpen, setIsLoginScreenOpen] = useState(true);
 
     // eslint-disable-next-line no-restricted-properties
     const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
+
+    function inputName(event) {
+        setName(event.target.value);
+    }
+
+    function closeLoginScreen() {
+        setIsLoginScreenOpen(false);
+    }
+
+    useEffect(() => {
+        if (name !== '') {
+            setIsLoginScreenOpen(false);
+        }
+    }, []);
 
     useEffect(() => {
         Notification.requestPermission();
     }, []);
 
+    useEffect(() => {
+        Cookies.set('level', String(level));
+        Cookies.set('currentExperience', String(currentExperience));
+        Cookies.set('challengesCompleted', String(challengesCompleted));
+    }, [level, currentExperience, challengesCompleted]);
+
     function levelUp() {
         setLevel(level + 1);
+        setIsLevelUpModalOpen(true);
+    }
+
+    function closeLevelUpModal() {
+        setIsLevelUpModalOpen(false);
     }
 
     function startNewChallenge() {
@@ -93,13 +138,19 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
                 challengesCompleted,
                 experienceToNextLevel,
                 levelUp,
+                name,
                 startNewChallenge,
                 activeChallenge,
                 resetChallenge,
                 completeChallenge,
+                closeLevelUpModal,
+                inputName,
+                closeLoginScreen,
             }}
         >
             {children}
+            {isLevelUpModalOpen && <LevelUpModal />}
+            {isLoginScreenOpen && <LoginScreen />}
         </ChallengesContext.Provider>
     );
 }
